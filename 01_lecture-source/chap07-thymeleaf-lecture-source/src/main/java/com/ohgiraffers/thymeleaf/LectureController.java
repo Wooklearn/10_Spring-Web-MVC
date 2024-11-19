9df21397136fb3b6fea44f420d4b7c85c4f08ac6
Yongwook <kyw9885@gmail.com>
2024-11-19 12:52:06 +0900

    [1119] thymeleaf

--- 01_lecture-source/chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
+++ 01_lecture-source/chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/lecture/*")
 public class LectureController {
@@ -28,6 +31,14 @@ public class LectureController {
 
         mv.addObject("num", 1);
         mv.addObject("str", "바나나");
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("하츄핑", 4, '여', "서울시 노진구"));
+        memberList.add(new MemberDTO("시진핑", 6, '남', "베이징 사천구"));
+        memberList.add(new MemberDTO("티니핑", 8, '남', "서울시 광진구"));
+        memberList.add(new MemberDTO("핑구", 4, '남', "서울시 핑구"));
+
+        mv.addObject("memberList", memberList);
 
         mv.setViewName("lecture/conditional");
 
